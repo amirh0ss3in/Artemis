f30780e2c5a96e93e9d5f6497dea2b4f5dcba0e5
amirh0ss3in <59564278+amirh0ss3in@users.noreply.github.com>
2020-09-03 19:26:41 +0430

    Update resnet_transfer_learning.py

--- resnet_transfer_learning.py
+++ resnet_transfer_learning.py
@@ -1,6 +1,3 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
-from keras.utils import to_categorical
 import keras
 from keras.models import Model
 from keras.layers import Dense,Flatten
@@ -10,48 +7,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import classification_report
 
-negative=np.load("/content/drive/My Drive/DCP/DCP_positive_b.npy")
-positive=np.load("/content/drive/My Drive/DCP/DCP_negative_b.npy")
-
-n=negative[:,10:15]
-p=positive[:,10:15]
-
-nn=list()
-pp=list()
-
-data=list()
-label=list()
-
-for i in n:
-  for j in i:
-    nn.append(j)
-
-for i in p:
-  for j in i:
-    pp.append(j)
-
-ny= np.zeros(len(nn),dtype=int)
-py= np.ones(len(pp),dtype=int)
-
-
-for i in nn:
-  data.append(i)
-for i in pp:
-  data.append(i)
-
-for i in ny:
-  label.append(i)
-for i in py:
-  label.append(i)
-
-data=np.array(data)
-label=np.array(label)
-label=to_categorical(label)
-
-trainX, testX, trainY, testY = train_test_split(
-    data, label, test_size=0.2, random_state=42)
-
-
+trainX=np.load("C:/Users/Amirhossein/Desktop/trainX.npy")
+testX=np.load("C:/Users/Amirhossein/Desktop/testX.npy")
+trainY=np.load("C:/Users/Amirhossein/Desktop/trainY.npy")
+testY=np.load("C:/Users/Amirhossein/Desktop/testY.npy")
 
 print(trainX.shape,testX.shape)
 
